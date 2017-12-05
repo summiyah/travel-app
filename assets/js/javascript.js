6e3ba5a1bc1fbb505a79e8013fb0110a628b7766
kelichau <tchau2@outlook.com>
2017-12-05 14:25:27 -0500

    Updated AJAX skeleton in js file

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,16 +6,26 @@ var queryURL = "";
 $.ajax({
 	url: queryURL,
 	method: "GET"
+	data: {} //flights api data
+
+$.ajax({
+	url: queryURL,
+	method: "GET"
+	data: {} //currency api data
+
+$.ajax({
+	url: queryURL,
+	method: "GET"
+	data: {} //weather api data
+
+$.ajax({
+	url: queryURL,
+	method: "GET"
+	data: {} //air quality api data
 })
-
-
-
-
-
-
-
-
-
+})
+})
+})
 
 
 });
