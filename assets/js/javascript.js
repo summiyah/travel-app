8f7356621990eba5dde57b044d655e0b814d1384
kelichau <tchau2@outlook.com>
2017-12-06 12:16:29 -0500

    JS Updates

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,18 +1,24 @@
 $(document).ready(function(){
     console.log("I'm ready!");
-
-    $(".placeholder").on("click", function(){
-		var weatherURL = "https://wunderground.com/search?y=" + weather + fahreinheit + celsius + temperature + precipitation + "=&plot=short&apikey=9e5b268d945691e1";
-
+    $("#weatherResults").on("click", function(result){
+    	console.log("Ok");
+		var weatherURL = "https://api.wunderground.com/api/9e5b268d945691e1/conditions/q/CA/San_Francisco.json";
+		console.log(weatherURL);
 		$.ajax({
 			url: weatherURL,
 			method: "GET"
+			dataType: "jsonp",
 		}).done(function(result){
 			console.log(result);
     	}).fail(function(err){
     		throw err;
     		console.log(err);
     	});
-	});
+    	var data = $("<weatherResults>");
+    	data.attr("result".data)
+    	$("#weatherInput").append(data)
+    	$(document).on("click", "button", weatherResults);
+    	console.log(data);
 
+	});
 });
