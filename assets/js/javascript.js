a89463900f4cf6d4f415cd0f1497a60585864f39
Juanita Pinkney <juanitapinkney@Juanitas-MacAir.attlocal.net>
2017-12-06 20:13:01 -0500

    adds travelchecklist page

---
+++ assets/js/javascript.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    console.log("I'm ready!");
+    $(".placeholder").on("click", function(weatherResults){
+		var weatherURL = "https://api.wunderground.com/api/9e5b268d945691e1/conditions/weather";
+		console.log(weatherURL);
+		$.ajax({
+			url: weatherURL,
+			method: "GET"
+		}).done(function(result){
+			console.log(result);
+    	}).fail(function(err){
+    		throw err;
+    		console.log(err);
+    	});
+    	var data = $("<weatherResults>");
+    	data.attr("result".data)
+    	$(#weatherResults).append(data)
+    	$(document).on("click", ".button", weatherResults);
+
+	});
+});
+
+
+
+var button = data target
+   var alert = myAlert
+   
+
+var 
+$('#myAlert').on('closed.bs.alert', function () {
+  // do something…
+})
+
+
+$().alert()
+
+$().alert('close')
