0281200fda0e9442e334f28a9cd962faacd2a553
Summiyah Siddeeq <summiyah@gmail.com>
2017-12-11 21:43:32 -0500

    added the currency API to the page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -50,4 +50,79 @@ $(document).ready(function(){
 
 });
 
+$(document).ready(function() {
+    console.log("I'm ready!");
+
+
+
+    var currencyQueryURL = "http://www.apilayer.net/api/live?access_key=3a9198bf3feebaee79f8a0513601a325";
+
+    var airQualityQueryURL = "https://api.waqi.info/feed/shanghai/?token=e08aa62141cab1b9ad7597b1af9ebdead16435fd";
+
+    var weatherURL = "https://wunderground.com/" + "&y=&plot=short&apikey=9e5b268d945691e1";
+
+    var submitButton = $('#add_topic');
+
+    var searchInput = $('#search_input');
+
+    var currencyConverter = $("currencyConverter");
+
+    console.log(submitButton)
+    $("#add_topic").on("click", function(event) {
+        event.preventDefault();
+        console.log("Hi my name is");
+
+        function destinationAjaxCall() {
+            var destination = $(this).attr("data-name");
+            var searchTerm = searchInput.val()
+            console.log("Hi my name is");
+            var destinationURL = "https://restcountries.eu/rest/v2/name/" + searchTerm + "?fullText=true"
+
+            $.ajax({
+                url: destinationURL,
+                method: "GET"
+            }).done(function(response) {
+
+                console.log(response)
+                console.log(response[0].languages[0].name)
+                var currencyCode = response[0].currencies[0].code;
+                var currencyName = response[0].currencies[0].name;
+                var languageName = response[0].languages[0].name;
+                console.log(response[0].currencies[0].code)
+                console.log(response[0].currencies[0].name)
+
+                $("#search-results").append("<p>" + currencyCode + "</p>")
+                $("#search-results").append("<p>" + currencyName + "</p>")
+                $("#search-results").append("<p>" + languageName + "</p>")
+                currencyAjaxCall(currencyCode);
+
+            });
+        }
+
+
+        function currencyAjaxCall(currencyCode) {
+            var currency = $(this).attr("data-name");
+            var searchCurrency = searchInput.val()
+            var currencyQueryURL = "http://www.apilayer.net/api/live?access_key=3a9198bf3feebaee79f8a0513601a325&currencies="+ currencyCode;
+
+            console.log("Hey you")
+
+            $.ajax({
+                url: currencyQueryURL,
+                method: "GET"
+            }).done(function(response) {
+                console.log(response.quotes["USD" + currencyCode])
+                $("#search-results").append("<p>" + response.quotes["USD"+ currencyCode] + "</p>")
+            });
+
+        }
+
+        destinationAjaxCall();
+
+    });
+
+});
+
+
+
 
